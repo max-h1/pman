29d337a617ef70c0cc0be412fda2a68cf43af449
Max Henley <rfd6296@autuni.ac.nz>
2024-10-31 12:49:57 +1300

    Set up mysql database config

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,16 +1,33 @@
+import os
 from flask import jsonify, request, Flask
 from flask_cors import CORS
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
- 
-mysql = MySQL(app)
+load_dotenv()
+
+db_user = os.environ.get('DB_USER')
+db_password = os.environ['DB_PASSWORD']
+db_name = os.environ['DB_NAME']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@localhost/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, to suppress warnings
+
+# Initialize the SQLAlchemy object
+db = SQLAlchemy(app)
+
+class Entry(db.Model):
+    __tablename__ = 'entries'
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(128), index=True)
+    user = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f'Entry: Service = {self.service} | User = {self.user}'
 
 CORS(app)
 
